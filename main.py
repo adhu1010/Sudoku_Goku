20b8fb60487c9a57b5c45890129a939b5553d424
Adhwaith K P <adhwaithatwork@gmail.com>
2023-09-10 11:02:26 +0530

    sudoku goku version 1

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import numpy as np
-grid=[[],[],[],[],[],[],[],[],[]]
-for i in range(1,10):
-    for j in range(1,10):
+grid=[]
+for i in range(0,9):
+    row=[]
+    for j in range(0,9):
         x=int(input("Enter the number  :"))
-        grid[i][j]=x
+        row.append(x)
+    grid.append(row)
         
 print(np.matrix(grid))
+def possible(y,x,n):
+    global grid
+    for i in range (9):
+        if grid[y][i]==n:
+            return False
+    for i in range(9):
+        if grid[i][x]==n:
+            return False
+    x0 = (x//3)*3
+    y0 = (y//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if grid[y0+i][x0+j] == n:
+                return False
+    return True
+
+def solve():
+    global grid
+    for y in range(9):
+        for x in range(9):
+            if grid [y][x]==0:
+                for n in range(1,10):
+                    if possible(y,x,n):
+                        grid[y][x]=n
+                        solve()
+                        grid[y][x]=0
+                return
+    print(np.matrix(grid))
+    input("more?")  
+                          
+solve()
